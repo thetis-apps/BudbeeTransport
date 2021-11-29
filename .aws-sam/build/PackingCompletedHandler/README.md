0380cb0c7502d66bd95ebe045c38e0c8baac8f47
thetis-apps <lmp@thetis-apps.com>
2021-11-29 14:20:51 +0000

    T

--- .aws-sam/build/PackingCompletedHandler/README.md
+++ .aws-sam/build/PackingCompletedHandler/README.md
@@ -1,10 +1,10 @@
 # Introduction
 
-This application enables the printing of shipping labels from the carrier Postnord as an integrated part of your packing process. 
+This application enables the printing of shipping labels from the carrier Budbee as an integrated part of your packing process. 
 
 # Installation
 
-You may install the latest version of the application from the Serverless Applicaation Repository. It is registered under the name thetis-ims-postnord-transport.
+You may install the latest version of the application from the Serverless Applicaation Repository. It is registered under the name thetis-ims-budbee-transport.
 
 ## Parameters
 
@@ -20,19 +20,18 @@ A short explanation for each of these parameters are provided upon installation.
 
 ## Initialization
 
-Upon installation the application creates a carrier by the name 'Postnord'.
+Upon installation the application creates a carrier by the name 'Budbee'.
 
 # Configuration
 
-In the data document of the carrier named 'Postnord':
+In the data document of the carrier named 'Budbee':
 ```
 {
-  "PostnordTransport": {
-    "pin": "U2YW3OMPKC3B5FB5FDF2BJD6",
+  "BudbeeTransport": {
     "test": true,
-    "user": "RAFRFWPPDI5PHJ5W",
-    "bulkId": "1",
-    "senderQuickId": "DEMO"
+    "apiKey": "d1c1aa0d-60a6-4b1d-9ab3-38066bb6ea51",
+    "apiSecret": "8bc90286-15b1-4059-ad86-bda16cf713d74fec3d7e-944d-4c65-ae51-9231591e1e4a",
+    "collectionId": 821
   }
 }
 ```
@@ -45,7 +44,7 @@ To get your own credentials contact Postnord.
 
 ## Packing completed
 
-When packing of a shipment is completed, the application registers the shipment with Postnord. The shipment is updated with the carriers shipment number.
+When packing of a shipment is completed, the application registers the shipment with Budbee. The shipment is updated with the carriers shipment number.
 
 The shipping containers are updated with the tracking numbers assigned to the corresponding Postnord packages.
 
---
+++ .aws-sam/build/PackingCompletedHandler/src/handlers/budbee-transport.js
@@ -0,0 +1,320 @@
+/**
+ * Copyright 2021 Thetis Apps Aps
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const axios = require('axios');
+
+var AWS = require('aws-sdk');
+AWS.config.update({region:'eu-west-1'});
+
+/**
+ * Send a response to CloudFormation regarding progress in creating resource.
+ */
+async function sendResponse(input, context, responseStatus, reason) {
+
+	let responseUrl = input.ResponseURL;
+
+	let output = new Object();
+	output.Status = responseStatus;
+	output.PhysicalResourceId = "StaticFiles";
+	output.StackId = input.StackId;
+	output.RequestId = input.RequestId;
+	output.LogicalResourceId = input.LogicalResourceId;
+	output.Reason = reason;
+	await axios.put(responseUrl, output);
+}
+
+exports.initializer = async (input, context) => {
+	
+	try {
+		let ims = await getIMS();
+		let requestType = input.RequestType;
+		if (requestType == "Create") {
+			let carrier = new Object();
+			carrier.carrierName = "Budbee";
+		    let setup = new Object();
+			setup.apiKey = "d1c1aa0d-60a6-4b1d-9ab3-38066bb6ea51";
+			setup.apiSecret = "8bc90286-15b1-4059-ad86-bda16cf713d74fec3d7e-944d-4c65-ae51-9231591e1e4a";
+			setup.test = true;
+			setup.collectionId = 821;
+			let dataDocument = new Object();
+			dataDocument.BudbeeTransport = setup;
+			carrier.dataDocument = JSON.stringify(dataDocument);
+			await ims.post("carriers", carrier);
+		}
+		await sendResponse(input, context, "SUCCESS", "OK");
+
+	} catch (error) {
+		await sendResponse(input, context, "SUCCESS", JSON.stringify(error));
+	}
+
+};
+
+async function getIMS() {
+	
+    const authUrl = "https://auth.thetis-ims.com/oauth2/";
+    const apiUrl = "https://api.thetis-ims.com/2/";
+
+	var clientId = process.env.ClientId;   
+	var clientSecret = process.env.ClientSecret; 
+	var apiKey = process.env.ApiKey;  
+	
+    let data = clientId + ":" + clientSecret;
+	let base64data = Buffer.from(data, 'UTF-8').toString('base64');	
+	
+	var imsAuth = axios.create({
+			baseURL: authUrl,
+			headers: { Authorization: "Basic " + base64data, 'Content-Type': "application/x-www-form-urlencoded" },
+			responseType: 'json'
+		});
+    
+    var response = await imsAuth.post("token", 'grant_type=client_credentials');
+    var token = response.data.token_type + " " + response.data.access_token;
+    
+    var ims = axios.create({
+    		baseURL: apiUrl,
+    		headers: { "Authorization": token, "x-api-key": apiKey, "Content-Type": "application/json" }
+    	});
+	
+
+	ims.interceptors.response.use(function (response) {
+			console.log("SUCCESS " + JSON.stringify(response.data));
+ 	    	return response;
+		}, function (error) {
+			console.log(JSON.stringify(error));
+			if (error.response) {
+				console.log("FAILURE " + error.response.status + " - " + JSON.stringify(error.response.data));
+			}
+	    	return Promise.reject(error);
+		});
+
+	return ims;
+}
+
+async function getBudbee(setup) {
+ 
+    let budbeeUrl;
+    if (setup.test) {
+    	budbeeUrl = "https://sandbox.api.budbee.com/";
+    } else {
+    	budbeeUrl = "https://api.budbee.com/";
+    }
+    
+    let authentication = setup.apiKey + ':' + setup.apiSecret;
+    var unifaun = axios.create({
+		baseURL: budbeeUrl, 
+		headers: { "Authorization": "Basic " + new Buffer.from(authentication).toString('base64') },
+		validateStatus: function (status) {
+		    return status >= 200 && status < 300 || status == 404; // default
+		}
+	});
+	
+	unifaun.interceptors.response.use(function (response) {
+			console.log("SUCCESS " + JSON.stringify(response.data));
+ 	    	return response;
+		}, function (error) {
+			if (error.response) {
+				console.log("FAILURE " + error.response.status + " - " + JSON.stringify(error.response.data));
+			}
+	    	return Promise.reject(error);
+		});
+
+	return unifaun;
+}
+
+function lookupCarrier(carriers, carrierName) {
+	let i = 0;
+    let found = false;
+    while (!found && i < carriers.length) {
+    	let carrier = carriers[i];
+    	if (carrier.carrierName == carrierName) {
+    		found = true;
+    	} else {
+    		i++;
+    	}	
+    }
+    
+    if (!found) {
+    	throw new Error('No carrier by the name ' + carrierName);
+    }
+
+	return carriers[i];
+}
+
+// Get setup from either carrier or from seller
+
+async function getSetup(ims, shipment) {
+	let setup;
+    let sellerId = shipment.sellerId;
+	if (sellerId != null) {
+	    let response = await ims.get("sellers/" + sellerId);
+	    let seller = response.data;
+	    let dataDocument = JSON.parse(seller.dataDocument);
+    	setup = dataDocument.BudbeeTransport;
+	} else {
+		let response = await ims.get("carriers");
+    	let carriers = response.data;
+	    let carrier = lookupCarrier(carriers, 'Budbee');
+	    let dataDocument = JSON.parse(carrier.dataDocument);
+	    setup = dataDocument.BudbeeTransport;
+	}
+	return setup;
+}
+
+
+exports.deliveryNoteCancelledHandler = async (event, context) => {
+
+    console.info(JSON.stringify(event));
+    
+    var detail = event.detail;
+    var shipmentId = detail.shipmentId;
+ 
+	let ims = await getIMS();
+	
+    let response = await ims.get("shipments/" + shipmentId);
+    let shipment = response.data;
+
+	if (shipment.carrierName == 'Budbee') {
+
+		let setup = await getSetup(ims, shipment);
+	
+		let budbee = await getBudbee(setup);
+
+		budbee.defaults.headers["Content-Type"] = "application/vnd.budbee.multiple.orders-v1+json";
+		await budbee.delete("multiple/orders/" + shipment.carriersShipmentNumber);
+		
+	}
+
+};
+
+/**
+ * A Lambda function that gets shipping labels from budbee.
+ */
+exports.packingCompletedHandler = async (event, context) => {
+	
+    console.info(JSON.stringify(event));
+
+    var detail = event.detail;
+    var shipmentId = detail.shipmentId;
+ 
+	let ims = await getIMS();
+	
+    let response = await ims.get("shipments/" + shipmentId);
+    let shipment = response.data;
+
+	let setup = await getSetup(ims, shipment);
+
+	let budbee = await getBudbee(setup);
+
+	let budbeeOrder = new Object();
+	budbeeOrder.collectionId = setup.collectionId;
+	budbeeOrder.cart = { cartId: shipment.shipmentNumber };
+	budbeeOrder.delivery = {
+		    "name": shipment.deliveryAddress.addressee,
+		    "referencePerson": shipment.deliveryAddress.careOf,
+		    "telephoneNumber": shipment.contactPerson.mobileNumber,
+		    "email": shipment.contactPerson.email,
+		    "address": {
+		      "street": shipment.deliveryAddress.streetNameAndNumber,
+		      "street2": shipment.deliveryAddress.districtOrCityArea,
+		      "postalCode": shipment.deliveryAddress.postalCode,
+		      "city": shipment.deliveryAddress.cityTownOrVillage,
+		      "country": shipment.deliveryAddress.countryCode
+		    },
+		    "doorCode": shipment.deliveryAddress.floorBlockOrSuite,
+		    "outsideDoor": true,
+		    "additionalInfo": shipment.notesOnDelivery
+		};
+  
+	budbeeOrder.requireSignature = false;
+	budbeeOrder.additionalServices = {
+	        "identificationCheckRequired": false,
+	        "recipientMinimumAge": 0,
+	        "recipientMustMatchEndCustomer": false,
+	        "numberOfMissRetries": null
+		};
+
+	let parcels = [];
+	let shippingContainers = [];
+	shippingContainers = shipment.shippingContainers;
+	shippingContainers.forEach(function(shippingContainer) {
+		let parcel = new Object();
+		let dimensions = shippingContainer.dimensions;
+		parcel.dimensions = {
+		        "width": dimensions.width * 10,
+		        "height": dimensions.height * 10,
+		        "length": dimensions.length * 10,
+		        "weight": shippingContainer.grossWeight * 1000,
+		        "volume": shippingContainer.volume * 10000
+		    };
+		parcels.push(parcel);
+	});
+	
+	budbeeOrder.parcels = parcels;
+
+	budbee.defaults.headers["Content-Type"] = "application/vnd.budbee.multiple.orders-v2+json";
+    response = await budbee.post("multiple/orders", budbeeOrder);
+
+	if (response.status == 404) {
+		
+		// Send error messages
+		
+		let error = response.data;
+		let message = new Object();
+		message.time = Date.now();
+		message.source = "BudbeeTransport";
+		message.messageType = "ERROR";
+		message.messageText = "Failed to register shipment with Budbee. Budbee says: " + error.message;
+		message.deviceName = detail.deviceName;
+		message.userId = detail.userId;
+		await ims.post("events/" + detail.eventId + "/messages", message);
+
+	} else {
+    
+    	// Set tracking number on shipping containers and attach labels to shipment
+
+		shippingContainers = shipment.shippingContainers;
+		let parcels = response.data.parcels;
+		for (let i = 0; i < response.data.parcels.length; i++) {
+			let shippingContainer = shippingContainers[i];
+			let parcel = parcels[i];
+			
+			let shippingLabel = new Object();
+			shippingLabel.fileName = "SHIPPING_LABEL_" + shipmentId + ".pdf";
+			shippingLabel.presignedUrl = parcel.label;
+			await ims.post("shipments/"+ shipmentId + "/attachments", shippingLabel);
+
+			await ims.patch("shippingContainers/" + shippingContainer.id, { trackingNumber: parcel.packageId });
+			
+		}
+		
+		// Set carriers shipment id
+		
+		await ims.patch("shipments/" + shipment.id, { carriersShipmentNumber: response.data.id });
+		
+		var message = new Object();
+		message.time = Date.now();
+		message.source = "BudbeeTransport";
+		message.messageType = "INFO";
+		message.messageText = "Labels are ready";
+		message.deviceName = detail.deviceName;
+		message.userId = detail.userId;
+		await ims.post("events/" + detail.eventId + "/messages", message);
+	
+	}
+
+	return "done";
+
+};
